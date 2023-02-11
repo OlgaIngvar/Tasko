9ecf36e89c0ae2302d0bd340810bd5ba921adac0
Olga <olgaingvar@gmail.com>
2023-02-11 14:39:43 +0200

    вход 3 числа для проверки существования треугольника со сторонами такой длины

---
+++ T12/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает на вход три числа и проверяет,
+ может ли существовать треугольник с сторонами такой длины.
+ Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.*/
+
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+int a = InputInt("Введите число а");
+int b = InputInt("Введите число b");
+int c = InputInt("Введите число c");
+
+bool Ygol(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+        return true;
+    else
+    {
+        return false;
+    }
+}
+if (Ygol(a, b, c))
+{
+    System.Console.WriteLine("является треугольником");
+
+}
+else
+{
+    System.Console.WriteLine("не является треугольником");
+}

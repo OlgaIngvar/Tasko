7b997606047eb6a8ec53af6410067d4615a2cb95
Olga <olgaingvar@gmail.com>
2023-02-12 13:28:46 +0200

    представление главной папке репозитория. потом как пушить подпапку

--- T0/Program.cs
+++ T0/Program.cs
@@ -3,3 +3,8 @@ git commit -m "first commit"
 git remote add origin https://github.com/OlgaIngvar/Znakomstvo_s_bazami_dannih.git
 git branch -M main
 git push -u origin main
+
+git add .
+git commit -m "краткое содержание папки"
+git push -u origin main
+

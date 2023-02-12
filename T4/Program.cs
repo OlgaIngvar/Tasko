9a4aa9fb97ee0a73e1f2bd9e3b293f765d185712
Olga <olgaingvar@gmail.com>
2023-02-12 10:36:23 +0200

    .

---
+++ T4/Program.cs
@@ -0,0 +1,26 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write($"{message}: ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int[] CreatArray(int X)
+{
+    int[] array = new int[X];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]}");
+    }
+}
+
+int X = Prompt("Size of array ");
+int [] array = CreatArray(X);
+ShowArray (array);
